349a3b30c041bfffad75ab92e09438964e9c79cf
Puviyarasan-U <45589516+Puviyarasan-U@users.noreply.github.com>
2019-02-13 12:50:12 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,19 +2,11 @@
 // module.exports={	
 //     user: 'sa',
 //     password: 'sql',
-//     server: 'DESKTOP-68OF7RJ', 
-//     database: 'Bizopsai'
+//     server: 'xxxxx', 
+//     database: 'yyyyy'
 // };
 
 //var config = 
 module.exports={ 
-     userName: 'bizops_user', // update me
-     password: 'Bizsql707', // update me
-     server: 'bizopsdev.database.windows.net', // update me
-     options: 
-        {
-           database: 'Bizopsai' //update me
-           , encrypt: true,
-           rowCollectionOnRequestCompletion: true
-        }
+    
    };
